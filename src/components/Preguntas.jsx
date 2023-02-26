457d46a862a37e049d4886e2bbda0b89ff29ebc6
vanf-x <facundoaguirre1989@gmail.com>
2023-02-26 15:51:04 -0300

    Add: completada la parte de las preguntas

--- src/components/Preguntas.jsx
+++ src/components/Preguntas.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { ProgressBar, Step } from "react-step-progress-bar";
 import "react-step-progress-bar/styles.css";
 
@@ -8,7 +8,20 @@ const Preguntas = () => {
   const [preguntas, setPreguntas] = useState([]);
   const [respuestas, setRespuestas] = useState([]);
   const [respondido, setRespondido] = useState(false);
-  const [colorRespuesta, setcolorRespuesta] = useState("");
+  const solucion = useRef();
+  const referencias = [useRef(), useRef(), useRef(), useRef()];
+
+  const resetearBackground = () => {
+    referencias.forEach((e) => {
+      if (e.current.classList.contains("bg-green-600")) {
+        e.current.classList.remove("bg-green-600");
+      }
+      if (e.current.classList.contains("bg-red-700/75")) {
+        e.current.classList.remove("bg-red-700/75");
+      }
+      e.current.classList.add("bg-[#0958b7]", "hover:bg-[#0c5fdd]");
+    });
+  };
 
   const resetearJuego = () => {
     setRespondido(false);
@@ -17,9 +30,21 @@ const Preguntas = () => {
     setRespuestas([]);
   };
 
+  const colorearSegunRespuesta = (e, respuesta) => {
+    if (respuesta === "Incorrecto") {
+      e.target.classList.remove("bg-[#0958b7]", "hover:bg-[#0c5fdd]");
+      e.target.classList.add("bg-red-700/75");
+    } else {
+      e.target.classList.remove("bg-[#0958b7]", "hover:bg-[#0c5fdd]");
+      e.target.classList.add("bg-green-600");
+    }
+  };
+
   const siguientePregunta = () => {
     if (index < 4) {
       setTimeout(() => {
+        resetearBackground();
+        solucion.current.classList.add("hidden");
         setIndex(index + 1);
         setRespondido(false);
       }, 1000);
@@ -32,6 +57,7 @@ const Preguntas = () => {
     if (respondido) {
       return;
     }
+    solucion.current.classList.remove("hidden");
     setRespondido(true);
     const opcionSeleccionada = e.target.innerHTML;
     const preguntaActual = preguntas[index];
@@ -40,7 +66,8 @@ const Preguntas = () => {
         ? "Correcto"
         : "Incorrecto";
     setRespuestas([...respuestas, respuesta]);
-    console.log(respuestas);
+
+    colorearSegunRespuesta(e, respuesta);
   };
 
   useEffect(() => {
@@ -62,56 +89,66 @@ const Preguntas = () => {
   }, [respuestas]);
 
   return (
-    <div className="m-5 bg-transparent">
+    <div className="m-5 bg-cyan-600 p-3">
       <div className="text-center">
-        <h1 className="capitalize text-xl text-white font-bold bg-[#0958b7]">
+        <h1 className="capitalize text-2xl text-white font-bold bg-[#0958b7]">
           {preguntas[index]?.categoria}
         </h1>
-        <h2 className="my-3">{preguntas[index]?.descripcion}</h2>
-        <div className="text-white">
+        <h2 className="my-3 text-white text-xl">{preguntas[index]?.descripcion}</h2>
+        <h2 className="my-3 text-white hidden" ref={solucion}>
+          Respuesta correcta: {preguntas[index]?.opcionCorrecta}
+        </h2>
+
+        <div className="text-white text-xl">
           <h3
-            className="bg-[#0958b7] hover:bg-[#0c5fdd] transition-colors my-3 mx-5 cursor-pointer"
+            className="bg-[#0958b7] hover:bg-[#0c5fdd] transition-colors my-4 mx-5 cursor-pointer rounded-xl p-1"
             onClick={definirCorrecta}
+            ref={referencias[0]}
           >
             {preguntas[index]?.opcionA}
           </h3>
           <h3
-            className="bg-[#0958b7] hover:bg-[#0c5fdd] transition-colors my-3 mx-5 cursor-pointer"
+            className="bg-[#0958b7] hover:bg-[#0c5fdd] transition-colors my-4 mx-5 cursor-pointer rounded-xl p-1"
             onClick={definirCorrecta}
+            ref={referencias[1]}
           >
             {preguntas[index]?.opcionB}
           </h3>
           <h3
-            className="bg-[#0958b7] hover:bg-[#0c5fdd] transition-colors my-3 mx-5 cursor-pointer"
+            className="bg-[#0958b7] hover:bg-[#0c5fdd] transition-colors my-4 mx-5 cursor-pointer rounded-xl p-1"
             onClick={definirCorrecta}
+            ref={referencias[2]}
           >
             {preguntas[index]?.opcionC}
           </h3>
           <h3
-            className="bg-[#0958b7] hover:bg-[#0c5fdd] transition-colors my-3 mx-5 cursor-pointer"
+            className="bg-[#0958b7] hover:bg-[#0c5fdd] transition-colors my-4 mx-5 cursor-pointer rounded-xl p-1"
             onClick={definirCorrecta}
+            ref={referencias[3]}
           >
             {preguntas[index]?.opcionD}
           </h3>
           <h3
-            className="bg-green-600 hover:bg-green-700 transition-colors my-3 mx-5 cursor-pointer"
+            className="bg-green-600 hover:bg-green-600 transition-colors my-4 mx-5 cursor-pointer"
             onClick={siguientePregunta}
           >
             Siguiente
           </h3>
         </div>
       </div>
-      <div className="progress-bar-container mx-10 text-xs text-white bg-gray-800">
+      <div className="progress-bar-container mx-10 text-xs text-white ">
         <ProgressBar
           percent={porcentaje}
+          unfilledBackground="rgba(0, 0, 255, 0.2)"
           filledBackground="linear-gradient(to right, #184684, #0150bb)"
           className="h-24"
+          height={20}
+          text="Puntaje"
         >
           <Step>
             {({ accomplished }) => (
               <div className="progress-step">
                 {accomplished ? <i className="fas fa-check"></i> : null}
-                <span>0%</span>
               </div>
             )}
           </Step>
@@ -119,7 +156,6 @@ const Preguntas = () => {
             {({ accomplished }) => (
               <div className="progress-step">
                 {accomplished ? <i className="fas fa-check"></i> : null}
-                <span>20%</span>
               </div>
             )}
           </Step>
@@ -127,7 +163,6 @@ const Preguntas = () => {
             {({ accomplished }) => (
               <div className="progress-step">
                 {accomplished ? <i className="fas fa-check"></i> : null}
-                <span>40%</span>
               </div>
             )}
           </Step>
@@ -135,7 +170,6 @@ const Preguntas = () => {
             {({ accomplished }) => (
               <div className="progress-step">
                 {accomplished ? <i className="fas fa-check"></i> : null}
-                <span>60%</span>
               </div>
             )}
           </Step>
@@ -143,7 +177,6 @@ const Preguntas = () => {
             {({ accomplished }) => (
               <div className="progress-step">
                 {accomplished ? <i className="fas fa-check"></i> : null}
-                <span>80%</span>
               </div>
             )}
           </Step>
@@ -151,7 +184,7 @@ const Preguntas = () => {
             {({ accomplished }) => (
               <div className="progress-step">
                 {accomplished ? <i className="fas fa-check"></i> : null}
-                <span>100%</span>
+                <span>{porcentaje}%</span>
               </div>
             )}
           </Step>
@@ -161,168 +194,3 @@ const Preguntas = () => {
   );
 };
 export default Preguntas;
-
-/*
-import React, { useEffect, useState } from "react";
-import { ProgressBar, Step } from "react-step-progress-bar";
-import "react-step-progress-bar/styles.css";
-
-const Preguntas = () => {
-  const [index, setIndex] = useState(0);
-  const [porcentaje, setPorcentaje] = useState(0);
-  const [preguntas, setPreguntas] = useState([]);
-  const [respuestas, setRespuestas] = useState([]);
-  const [respondido, setRespondido] = useState(false);
-
-  const resetearJuego = () => {
-    setRespondido(false);
-    setPorcentaje(0);
-    setPreguntas([]);
-    setRespuestas([]);
-  };
-
-  const siguientePregunta = () => {
-    if (index < 4) {
-      setTimeout(() => {
-        setIndex(index + 1);
-        setRespondido(false);
-      }, 1000);
-      return;
-    }
-    resetearJuego();
-  };
-
-  const definirCorrecta = (e) => {
-    if (respondido) {
-      return;
-    }
-    setRespondido(true);
-    const opcionSeleccionada = e.target.innerHTML;
-    const preguntaActual = preguntas[index];
-    const respuesta =
-      opcionSeleccionada === preguntaActual.opcionCorrecta
-        ? "Correcto"
-        : "Incorrecto";
-    setRespuestas([...respuestas, respuesta]);
-    console.log(respuestas);
-  };
-
-  useEffect(() => {
-    const url = "http://localhost:8080/api/juego/preguntas";
-    const peticion = fetch(url);
-    peticion
-      .then((datos) => datos.json())
-      .then((lectura) => {
-        setPreguntas(lectura);
-      });
-  }, []);
-
-  useEffect(() => {
-    const numCorrectas = respuestas.filter(
-      (respuesta) => respuesta === "Correcto"
-    ).length;
-    const porcentajeCalculado = (numCorrectas / 5) * 100;
-    setPorcentaje(porcentajeCalculado);
-  }, [respuestas]);
-
-  return (
-    <div className="m-5 bg-transparent">
-      <div className="text-center">
-        <h1 className="capitalize text-xl text-white font-bold bg-[#0958b7]">
-          {preguntas[index]?.categoria}
-        </h1>
-        <h2 className="my-3">{preguntas[index]?.descripcion}</h2>
-        <div className="text-white">
-          <h3
-            className="bg-[#0958b7] hover:bg-[#0c5fdd] transition-colors my-3 mx-5 cursor-pointer"
-            onClick={definirCorrecta}
-          >
-            {preguntas[index]?.opcionA}
-          </h3>
-          <h3
-            className="bg-[#0958b7] hover:bg-[#0c5fdd] transition-colors my-3 mx-5 cursor-pointer"
-            onClick={definirCorrecta}
-          >
-            {preguntas[index]?.opcionB}
-          </h3>
-          <h3
-            className="bg-[#0958b7] hover:bg-[#0c5fdd] transition-colors my-3 mx-5 cursor-pointer"
-            onClick={definirCorrecta}
-          >
-            {preguntas[index]?.opcionC}
-          </h3>
-          <h3
-            className="bg-[#0958b7] hover:bg-[#0c5fdd] transition-colors my-3 mx-5 cursor-pointer"
-            onClick={definirCorrecta}
-          >
-            {preguntas[index]?.opcionD}
-          </h3>
-          <h3
-            className="bg-green-600 hover:bg-green-700 transition-colors my-3 mx-5 cursor-pointer"
-            onClick={siguientePregunta}
-          >
-            Siguiente
-          </h3>
-        </div>
-      </div>
-      <div className="progress-bar-container mx-10 text-xs text-white bg-gray-800">
-        <ProgressBar
-          percent={porcentaje}
-          filledBackground="linear-gradient(to right, #184684, #0150bb)"
-          className="h-24"
-        >
-          <Step>
-            {({ accomplished }) => (
-              <div className="progress-step">
-                {accomplished ? <i className="fas fa-check"></i> : null}
-                <span>0%</span>
-              </div>
-            )}
-          </Step>
-          <Step>
-            {({ accomplished }) => (
-              <div className="progress-step">
-                {accomplished ? <i className="fas fa-check"></i> : null}
-                <span>20%</span>
-              </div>
-            )}
-          </Step>
-          <Step>
-            {({ accomplished }) => (
-              <div className="progress-step">
-                {accomplished ? <i className="fas fa-check"></i> : null}
-                <span>40%</span>
-              </div>
-            )}
-          </Step>
-          <Step>
-            {({ accomplished }) => (
-              <div className="progress-step">
-                {accomplished ? <i className="fas fa-check"></i> : null}
-                <span>60%</span>
-              </div>
-            )}
-          </Step>
-          <Step>
-            {({ accomplished }) => (
-              <div className="progress-step">
-                {accomplished ? <i className="fas fa-check"></i> : null}
-                <span>80%</span>
-              </div>
-            )}
-          </Step>
-          <Step>
-            {({ accomplished }) => (
-              <div className="progress-step">
-                {accomplished ? <i className="fas fa-check"></i> : null}
-                <span>100%</span>
-              </div>
-            )}
-          </Step>
-        </ProgressBar>
-      </div>
-    </div>
-  );
-};
-export default Preguntas;
-*/
